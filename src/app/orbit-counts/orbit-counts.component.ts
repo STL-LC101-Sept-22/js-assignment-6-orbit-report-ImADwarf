24b191103cca589b5c28c22736526c844559785c
Jonathan Cruzen <jonathancruzen@gmail.com>
2022-12-14 12:41:39 -0600

    Modified multiple files to add functionality to webpage

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -19,9 +19,10 @@ export class OrbitCountsComponent implements OnInit {
 	let count = 0;
 	if (this.satellites) {
 	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
-		 }
+		count++;
+		//  if (this.satellites[i].type === type) {
+		// 	count++;
+		//  }
 	  }
 	}
 	return count;
